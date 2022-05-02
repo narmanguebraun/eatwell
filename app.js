cd4a0dc82d4d63d4bb71d566e2e7b9b8734a7227
narmanguebraun <narmanguebraun1@gmail.com>
2022-05-02 22:24:47 +0200

    parse form data, redirect requests

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
-const path = require('path');
-
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   const htmlFilePath = path.join(__dirname, 'views', 'index.html');
   res.sendFile(htmlFilePath)
 });
 
-app.get('/restaurants', function(req, res){
+app.get('/restaurants', function(req, res) {
   const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
   res.sendFile(htmlFilePath)
 });
 
-app.get('/recommend', function(req, res){
+app.get('/recommend', function(req, res) {
   const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
   res.sendFile(htmlFilePath)
 });
 
-app.get('/confirm', function(req, res){
+app.post('/recommend', function(req,res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+  
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect('/confirm');
+});
+
+app.get('/confirm', function(req, res) {
   const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
   res.sendFile(htmlFilePath)
 });
 
-app.get('/about', function(req, res){
+app.get('/about', function(req, res) {
   const htmlFilePath = path.join(__dirname, 'views', 'about.html');
   res.sendFile(htmlFilePath)
 });
