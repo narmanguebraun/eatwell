e5eb5906bdb05ece2feee80bf87fd811a4b96ee5
narmanguebraun <narmanguebraun1@gmail.com>
2022-05-04 13:45:26 +0200

    split route config with Express router

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // built-in packages
-const fs = require('fs');
 const path = require('path');
 // third-party packages
 const express = require('express');
-const uuid = require('uuid');
+
 // own files: restaurants data
-const resData = require('./util/restaurant-data');
+const defaultRoutes = require('./routes/default');
+const restaurantRoutes = require('./routes/restaurants.js');
 
 const app = express();
 
@@ -15,56 +15,9 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  res.render('index');
-});
+app.use('/', defaultRoutes);
+app.use('/', restaurantRoutes);
 
-app.get('/restaurants', function(req, res) {
-  const storedRestaurants = resData.getStoredRestaurants();
-
-  res.render('restaurants', { 
-    numberOfRestaurants: storedRestaurants.length, 
-    restaurants: storedRestaurants, 
-  });
-});
-
-app.get('/restaurants/:id', function(req, res) {
-  const restaurantId = req.params.id;
-  const storedRestaurants = resData.getStoredRestaurants();
-
-  for (const restaurant of storedRestaurants) {
-    if (restaurant.id === restaurantId) {
-      return res.render('restaurant-detail', { restaurant: restaurant });
-    } // return to force to stop function execution
-  }
-
-  res.status(404).render('404') // no need to add return. the function stops after this.
-  
-});
-
-app.get('/recommend', function(req, res) {
-  res.render('recommend');
-});
-
-app.post('/recommend', function(req,res) {
-  const restaurant = req.body;
-  restaurant.id = uuid.v4();
-  const restaurants = resData.getStoredRestaurants();
-
-  restaurants.push(restaurant);
-  
-  resData.storedRestaurants(restaurants);
-
-  res.redirect('/confirm');
-});
-
-app.get('/confirm', function(req, res) {
-  res.render('confirm');
-});
-
-app.get('/about', function(req, res) {
-  res.render('about');
-});
 // technically correct, add status to update res object
 app.use(function(req, res) {
   res.status(404).render('404');
